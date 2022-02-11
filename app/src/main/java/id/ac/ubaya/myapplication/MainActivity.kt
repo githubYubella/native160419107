1dcc1428f719ae8eaf17d5accabcce7ff1709a50
kiky <kikyfadhilah2001@gmail.com>
2022-02-11 09:08:55 +0700

    hello world satu

--- app/src/main/java/id/ac/ubaya/myapplication/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/myapplication/MainActivity.kt
@@ -4,9 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    //coba hello world
+    fun main(args : Array<String>) {
+        println("Hello, World!")
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //dddd
     }
+
+
 }
